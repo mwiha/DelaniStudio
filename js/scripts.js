d9658f7c8393943c041514b1210397287d36586f
Alice Mwihaki <alicemwihaki99@gmail.com>
2019-11-09 11:45:03 +0300

    styled the work section

--- js/scripts.js
+++ js/scripts.js
@@ -1,92 +1,24 @@
- $(document).ready(function() {
-    //Design toggle
-    $(".design").click(function() {
-      $(".design-par").show();
-      $(this).hide();
-    })
-    $(".design-par").click(function() {
-      $(".design").show();
-      $(this).hide();
-    })
-    //Development toggle
-    $(".development").click(function() {
-      $(".development-par").show();
-      $(this).hide();
-    })
-     $(".development-par").click(function() {
-      $(".development").show();
-      $(this).hide();
-    })
-    // product management
-  $(".product").click(function() {
-    $(".product-par").show();
-    $(this).hide();
-  })
-  $(".product-par").click(function() {
-    $(".product").show();
-    $(this).hide();
-  })
-  //Portfolio hover effects
-  //##### Portfolio item 1 ####
-  $(".image").mouseenter(function() {
-    $(".para-1").show();
-  })
-  $(".image").mouseleave(function() {
-    $(".para-1").hide();
-  })
-  $(".image1").mouseenter(function() {
-    $(".para-2").show();
-  })
-  $(".image1").mouseleave(function() {
-    $(".para-2").hide();
-  })
-  $(".image2").mouseenter(function() {
-    $(".para-3").show();
-  })
-  $(".image2").mouseleave(function() {
-    $(".para-3").hide();
-  })
-  $(".image3").mouseenter(function() {
-    $(".para-4").show();
-  })
-  $(".image3").mouseleave(function() {
-    $(".para-4").hide();
-  })
-  $(".image4").mouseenter(function() {
-    $(".para-5").show();
-  })
-  $(".image4").mouseleave(function() {
-    $(".para-5").hide();
-  })
-  $(".image5").mouseenter(function() {
-    $(".para-6").show();
-  })
-  $(".image5").mouseleave(function() {
-    $(".para-6").hide();
-  })
-  $(".image6").mouseenter(function() {
-    $(".para-7").show();
-  })
-  $(".image6").mouseleave(function() {
-    $(".para-7").hide();
-  })
-  $(".image7").mouseenter(function() {
-    $(".para-8").show();
-  })
-  $(".image7").mouseleave(function() {
-      $(".para-8").hide();
-    })
-    // submit button
-  $('#submit').click(function(event) {
-    event.preventDefault();
-    var name = $('#name').val();
-    var email = $('#email').val();
-    var message = $('#message');
-    var key = '43ab10333d46e19325f96d210ed40c49-us5';
-    if (name == '' || email == '' || message == '') {
-      alert('Please make sure you have filled in the form correctly!');
-    } else {
-      alert(' Hi ' + name + ' We have received your message. Thank you for reaching out to us.');
-    }
-  });
-})
+document.getElementById("image1").addEventListener("click",function() {
+    document.querySelector("#image1").style.display="none";
+    document.querySelector("#design").style.display="block";
+});
+document.querySelector("#design").addEventListener("click",function() {
+    document.querySelector("#design").style.display="none";
+    document.querySelector("#image1").style.display="block";
+});
+document.getElementById("image2").addEventListener("click", function() {
+    document.querySelector("#image2").style.display = "none";
+    document.querySelector("#development").style.display = "block";
+});
+document.querySelector("#development").addEventListener("click", function() {
+    document.querySelector("#development").style.display = "none";
+    document.querySelector("#image2").style.display = "block";
+});
+document.getElementById("image3").addEventListener("click", function() {
+    document.querySelector("#image3").style.display = "none";
+    document.querySelector("#product").style.display = "block";
+});
+document.querySelector("#product").addEventListener("click", function() {
+    document.querySelector("#product").style.display = "none";
+    document.querySelector("#image3").style.display = "block";
+});
